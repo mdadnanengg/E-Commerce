08f683fe97a1cdac080acc0788387d793c4e2d8a
Mohammad Adnan <mdadnanengg@gmail.com>
2023-04-02 11:22:41 +0530

    Update Card.js

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,47 +3,49 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 
 function CardComponent() {
-  const [productData, setProductData] = useState({})
-  console.log(productData)
+  const [productData, setProductData] = useState([])
+  console.log({productData})
   useEffect(()=>{
     axios
-  .get("https://dummyjson.com/products/1")
-  .then((res) => setProductData(res.data))
+  .get("https://dummyjson.com/products/")
+  .then((res) => setProductData(res.data.products))
   .catch((err) => console.log("Something Went Wrong"))
   },[])
+
   return (
     <>
       <Grid container justifyContent='space-evenly'>
         <Grid item sx={10} sm={10} md={10}>
-          <Grid container spacing={2} justifyContent='space-around'>
+          <Grid container spacing={2} justifyContent='start'>
             {/* Cart Start */}
-            <Grid item xs={6} md={3} sx={{ display: 'flex', justifyContent: "center" }}>
+            { productData.map((item, value)=>{
+              return <Grid item xs={6} md={3} sx={{ display: 'flex', justifyContent: "center" }}>
               <Box sx={{ width: "250px" }}>
                 <Card >
                   <CardMedia
                     title='image'
                     component='img'
                     height='140'
-                    image={productData.thumbnail}
+                    image={item.thumbnail}
                   ></CardMedia>
                   <CardContent>
-                    <Typography gutterBottom variant='h5' component='div'> {productData.title} </Typography>
+                    <Typography gutterBottom variant='h5' component='div' sx={{height:"100px"}}> {item.title} </Typography>
                     <Box
                       sx={{
                         display: 'flex',
                         alignItems: 'center'
                       }}
                     >
-                      <Box sx={{ fontSize: '18px' }} color='#2A88AD'>{productData.rating}</Box>
-                      <Box><Rating name="rating" value={productData.rating} readOnly /></Box>
+                      <Box sx={{ fontSize: '18px' }} color='#2A88AD'>{item.rating}</Box>
+                      <Box><Rating name="rating" value={item.rating} readOnly /></Box>
                     </Box>
                     <Typography variant='subtitle1' color='textSecondary'>
-                      <Box component="span" fontWeight="bold" fontSize='150%' color='black'>${Math.floor(productData.price - (productData.price * (productData.discountPercentage / 100)))}</Box>
+                      <Box component="span" fontWeight="bold" fontSize='150%' color='black'>${item.price - (item.price * (item.discountPercentage / 100)).toFixed()}</Box>
                       <>
                         {' '}
-                        <Box component="span" color="text.secondary" textDecoration="line-through"><del>{productData.price}</del></Box>
+                        <Box component="span" color="text.secondary" textDecoration="line-through"><del>{item.price}</del></Box>
                         {' '}
-                        <Box component="span" color="error.main">({productData.discountPercentage}% off)</Box>
+                        <Box component="span" color="error.main">({item.discountPercentage}% off)</Box>
                       </>
                     </Typography>
                   </CardContent>
@@ -53,6 +55,7 @@ function CardComponent() {
                 </Card>
               </Box>
             </Grid>
+            }) }
           </Grid>
         </Grid>
       </Grid>
